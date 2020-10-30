743ca093166cdd6481a43d234400fb87a93a4a48
NickCuso <hardlydiff@gmail.com>
2020-10-29 20:44:04 -0700

    +deploy

---
+++ .circleci/scripts/deploy-ghpages.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+cd ../../
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo 'hardlydifficult.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:nickcuso/hardlydifficult.git master:gh-pages
+
+cd -
