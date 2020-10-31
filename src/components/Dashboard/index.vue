5263122b907f40a1f3b0a94f4192816528fabd22
NickCuso <hardlydiff@gmail.com>
2020-10-30 23:13:33 -0700

    Tighten box display

--- src/components/Dashboard/index.vue
+++ src/components/Dashboard/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="padding: 1em">
+  <div style="padding: 1em; display: inline-block">
     <span class="bold">NFT Market</span>
     <RecentTransactions network="xdai" address="0xE8Bfc092Df01243A3f8F6990e22454bcaF44C774" />
   </div>
