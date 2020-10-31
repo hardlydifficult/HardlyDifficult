3532efb46c9fab27be3066ba2d2881f01b1942e6
NickCuso <hardlydiff@gmail.com>
2020-10-30 23:27:59 -0700

    Link contract

--- src/components/Dashboard/index.vue
+++ src/components/Dashboard/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="padding: 1em; display: inline-block">
-    <span class="title">NFT Market</span>
+    <span class="title"><a href="https://blockscout.com/poa/xdai/address/0xE8Bfc092Df01243A3f8F6990e22454bcaF44C774">NFT Market</a></span>
     <RecentTransactions network="xdai" address="0xE8Bfc092Df01243A3f8F6990e22454bcaF44C774" />
   </div>
 </template>
